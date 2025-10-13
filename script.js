6c57e0b9a0c488407d00967e5cb7a05b703c3142
xoleric <XolEricsson@gmail.com>
2025-10-14 01:32:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+particlesJS("particles", {
+  particles: {
+    number: { value: 60 },
+    size: { value: 3 },
+    color: { value: "#00e0ff" },
+    line_linked: {
+      enable: true,
+      distance: 160,
+      color: "#00e0ff",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 2
+    }
+  }
+});
